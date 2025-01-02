76e9837e097f568ce8c2af3ef38138d5ce18a1cd
Skayear <skayearjbueno@hotmail.com>
2025-01-02 15:04:45 -0300

    Add new exercises 1 - Api and frontend

---
+++ Exercises 1/Frontend/ClientApp/proxy.conf.js
@@ -0,0 +1,21 @@
+const { env } = require('process');
+
+// const target = env.ASPNETCORE_HTTPS_PORT ? `http://api:${env.ASPNETCORE_HTTPS_PORT}` :
+//  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5000';
+
+const target = 'http://api:5146'
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+   ],
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  }
+]
+
+module.exports = PROXY_CONFIG;
